5d5d1a18e981b31b83d4b8a7d9e604bd53609c4c
rdoria96 <rddm096@gmail.com>
2023-05-03 12:20:28 -0500

    creacion de services,interfaces y componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SegurosComponent } from './component/seguros/seguros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SegurosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
