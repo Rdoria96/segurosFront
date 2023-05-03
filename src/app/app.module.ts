9f0a3d96868a0a5128523fa466b6b80c08b89ba3
Carlos Del Castillo M <maussac13@gmail.com>
2023-05-03 12:06:00 -0500

    Reconfiguracion de Front-end para el uso de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { OwnerComponent } from './owner/owner.component';
+import { PetComponent } from './pet/pet.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarModule } from './navbar/navbar.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OwnerComponent,
+    PetComponent,
+    HomeComponent,
+    LayoutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    LayoutModule,
+    ReactiveFormsModule,
+    NavbarModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
